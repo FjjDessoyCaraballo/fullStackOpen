7473ca54bdbea3a907b9a71a0104ee050047e1b6
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-08-15 13:45:35 +0300

    feat: exercise 1.11 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const StatisticLine = ({value, text}) => {
   return (
-    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleBadCounter = () => {
+    const updatedCounter = bad + 1
+    setBad(updatedCounter)
+    setAll(good + neutral + updatedCounter)
+  }
+  const handleNeutralCounter = () => {
+    const updatedCounter = neutral + 1
+    setNeutral(updatedCounter)
+    setAll(good + bad + updatedCounter)
+  }
+  const handleGoodCounter = () => {
+    const updatedCounter = good + 1
+    setGood(updatedCounter)
+    setAll(neutral + bad + updatedCounter)
+  }
+
+  const handleAverage = () => (good - bad) / all
+  const handlePercentage = () => ((good / all) * 100) + " %"
+  
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodCounter} text="good" />
+      <Button onClick={handleNeutralCounter} text="neutral"/>
+      <Button onClick={handleBadCounter} text="bad"/>
+      <h1>statistics</h1>
+      <li>No feedback given</li>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  )}
+  return (
+    <div>
+
+    <h1>give feedback</h1>
+    <Button onClick={handleGoodCounter} text="good" />
+    <Button onClick={handleNeutralCounter} text="neutral"/>
+    <Button onClick={handleBadCounter} text="bad"/>
+    <h1>statistics</h1>
+    <table>
+      <StatisticLine value={good} text="good"/>
+      <StatisticLine value={neutral} text="neutral"/>
+      <StatisticLine value={bad} text="bad"/>
+      <StatisticLine value={all} text="all" />
+      <StatisticLine value={handleAverage()} text="average" />
+      <StatisticLine value={handlePercentage()} text="positive" />
+    </table>
+    </div>
   )
 }
 
