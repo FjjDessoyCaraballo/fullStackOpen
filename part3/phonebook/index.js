01fbbc0c0a0fff431a8ba6f8908dc4aeae4a474b
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-09-17 11:41:53 +0300

    feat: exercises 3.7 and 3.8 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
-app.use(express.json())
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+// app.use(requestLogger)
 
+function getRandomInt(max) { return Math.floor(Math.random() * max) }
+
+const generateId = () => {
+    const newId = getRandomInt(2147483646)
+    return newId
+}
+
+morgan.token('personObject', (request) => { return request.createdPerson ? JSON.stringify(request.createdPerson) : '' })
+morgan.token('id', (request) => { return request.createdPerson?.id ? request.createdPerson.id.toString() : '' })
+
+app.use(express.json())
+app.use(morgan(':method :url :status :id - :response-time ms :personObject'))
 
 let persons = 
 [
@@ -27,13 +48,6 @@ let persons =
       "number": "39-23-6423122"
     }
 ]
-
-function getRandomInt(max) { return Math.floor(Math.random() * max) }
-
-const generateId = () => {
-    const newId = getRandomInt(2147483646)
-    return newId
-}
 
 app.get('/', (request, response) => {response.json("Status: OK").send()})
 
@@ -60,21 +74,23 @@ app.get('/api/info', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-    const body = request.query
+    const body = request.body
+    console.log(body)
     
     if (!body.name || !body.number) {
         return response.status(400).json({error: 'content missing'})
     }
     
     const match = persons.find(p => p.name === body.name)
-    if (match) return response.json("error: 'name must be unique'").end()
+    if (match) return response.status(400).json({error: 'name must be unique'})
 
     const newPerson = {
-        id: generateId(),
+        id: generateId().toString(),
         name: body.name,
         number: body.number,
     }
-    console.log(newPerson)
+
+    request.createdPerson = newPerson
     persons = persons.concat(newPerson)
     response.json(newPerson)
 })
@@ -84,7 +100,7 @@ app.delete('/api/persons/:id', (request, response) => {
     const person = persons.find(p => p.id === id)   
 
     if (person) {
-        persons = persons.filter(p => p.id === id)    
+        persons = persons.filter(p => p.id !== id)    
         response.status(204).end()
     }
     else response.status(404).end()
